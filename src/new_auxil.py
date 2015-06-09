0523be6417c2809e6ef8819816dac70bb8dc0903
jake <retallick.jake@gmail.com>
2015-06-09 11:20:26 -0700

    New auxil script (new_auxil.py) to better mesh with new_parse_qca

---
+++ src/new_auxil.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#---------------------------------------------------------
+# Name: auxil.py
+# Purpose: Auxiliary commonly used functions
+# Author:	Jacob Retallick
+# Created: 09.06.2014
+# Last Modified: 09.06.2015
+#---------------------------------------------------------

0cd537b9b7c90d347ac9f24c307515e2a6d9e5a6
jake <retallick.jake@gmail.com>
2015-06-11 10:25:04 -0700

    Added Solution class for handling solver results: solution.py

---
+++ src/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#---------------------------------------------------------
+# Name: solution.py
+# Purpose: Container file for Solution class and associated functions
+# Author:	Jacob Retallick
+# Created: 10.06.2014
+# Last Modified: 10.06.2015
+#---------------------------------------------------------
+
+
+class Solution:
+    '''A Solution object is constructed from the results of running a solver on
+    a QCA circuit. It is assumed that the solver approach is to find the
+    spectra of each clock zone for all possible inputs. The Solution object
+    contains methods to analyse these results in order to determine propoerties
+    of the whole circuit.'''
+
+    def __init__(self):
+        '''Initialise a Solution object'''
+        pass
